8f7683a7333d03113d7b87a3767072a0c54290e1
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-03-29 02:52:27 +0600

    first 2 page ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,47 @@ $(document).ready(function(){
         $(".bars").toggle();
         $(".close").toggle();
     });
+    $('#show-more').on('click', function() {
+        $('.doubleroom-row').toggle();
+        
+        var isVisible = $('.doubleroom-row').is(':visible');
+        
+        if (isVisible) {
+            $(".down-arrow").css("transform", "rotate(180deg)");
+        } else {
+            $(".down-arrow").css("transform", "rotate(0deg)");
+        }
+    });
+    $('#show-moreFamillyRoom').on('click', function() {
+        $('.famillyroom-row').toggle();
+        
+        var isVisible = $('.famillyroom-row').is(':visible');
+        
+        if (isVisible) {
+            $(".down-arrow2").css("transform", "rotate(180deg)");
+        } else {
+            $(".down-arrow2").css("transform", "rotate(0deg)");
+        }
+    });
+    $('#show-morestandardRoom').on('click', function() {
+        $('.standardroom-row').toggle();
+        
+        var isVisible = $('.standardroom-row').is(':visible');
+        
+        if (isVisible) {
+            $(".down-arrow3").css("transform", "rotate(180deg)");
+        } else {
+            $(".down-arrow3").css("transform", "rotate(0deg)");
+        }
+    });
+
+    $('#room1 td:nth-child(n+4):nth-child(-n+4)::after').attr('id', 'roomOneperson');
+
+
+    // Now you can use $('#generatedContent') to select this content
+    $('#roomOneperson').click(function() {
+        $("#reservationPersonDetailsOpen").show();
+    });
+    
+      
 });
