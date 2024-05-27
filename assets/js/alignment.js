dd5def28153a421368f2774abc3e22a75de89f6a
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-05-28 01:50:38 +0600

    hgfhfghfg

---
+++ assets/js/alignment.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+   
+    $('.alignment-button').click(function () {
+        $('.alignment-button').removeClass('alignmentactive'); // Remove active class from all buttons
+        $(this).addClass('alignmentactive'); // Add active class to clicked button
+    });
+    $(".horizontal-button").click(function(){
+        $(".verical-check-in-content").css({"display":"column"});
+        $(".vertical-content-item").css({"width":"100%"});
+        $(".vertical-content-body").css({
+            "flex-direction":"row",
+            "height":"430px",
+            "column-gap":"10px",
+            "row-gap":"10px"
+        });
+    });
+    $(".vertical-button").click(function(){
+        $(".verical-check-in-content").css({"flex-direction":" row"});
+        $(".vertical-content-item").css({"width":" 344px "});
+        $(".vertical-content-body").css({
+            "flex-direction":" column ",
+            "height":"550px ",
+            "column-gap":"10px",
+            "row-gap":"10px"
+        });
+    });
+    $(".arival-alignment-button").click(function(){
+        $(".arival-vertical").toggle();
+    $(".arival-horizontal").toggle();
+    });
+});
+//"verical-check-in-content"
